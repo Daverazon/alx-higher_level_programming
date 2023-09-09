facade9611b1344f74ff6c33a6a28dc0bdb10214
David <kamsidavid2@gmail.com>
2023-09-09 12:10:21 +0000

    corrections

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     lis = []
     for c in my_string:
-        if (c != "c" or c != "C"):
+        if (c != "c" and c != "C"):
             lis.append(c)
     return ("".join(lis))
