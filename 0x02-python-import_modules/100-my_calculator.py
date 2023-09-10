fee8de7012cc83afab98bdbf3c1cf4d1d253aaba
DavidI <kamsidavid2@gmail.com>
2023-09-11 00:00:38 +0100

    remove indent issues

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,12 +5,10 @@ if (__name__ == "__main__"):
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     for sign in ('+', '-', '*', '/'):
         if sign == argv[2]:
-            print("{} {} {} = {}".format(argv[1], argv[2],\
-                    argv[3], eval(f"{argv[1]} {argv[2]} {argv[3]}"))
+            result = eval(f"{argv[1]} {argv[2]} {argv[3]}")
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
         else:
-            print("Unknown operator. Available operators:"
-                " +, -, * and /")
+            print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
