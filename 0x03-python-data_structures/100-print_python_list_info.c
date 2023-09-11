b7a6927c0d435aa37d867f5683fd8791b60e3ebb
DavidI <kamsidavid2@gmail.com>
2023-09-11 23:00:28 +0100

    temp solution to ottask 14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_python_list_info - prints some basic info about python lists
+ * @p: 
+ * Return: nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+}
