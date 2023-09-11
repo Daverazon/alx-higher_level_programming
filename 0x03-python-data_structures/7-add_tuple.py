05edc08cfe0b6ddf6f061b5d79097a82d2b78050
David <kamsidavid2@gmail.com>
2023-09-11 14:05:36 +0000

    handle tuple with more than 2 numbers

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,4 +8,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0,)
     if (len(tuple_b) == 1):
         tuple_b += (0,)
+    if (len(tuple_a) > 2):
+        tuple_a = (tuple_a[0], tuple_a[1])
+    if (len(tuple_b) > 2):
+        tuple_b = (tuple_b[0], tuple_b[1])
     return (tuple(a + b for a, b in zip(tuple_a, tuple_b)))
