d0e76fbb68ba72ac8ea357d661451c6ac7a78719
David <kamsidavid2@gmail.com>
2023-09-11 17:14:46 +0000

    complete task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,27 +9,41 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *current = *head;
-	int *arr;
+	int count = 0, x = 0;
+	int *arr = NULL;
 
-	if (!head)
+	if (head == NULL)
 		return (0);
-	if (!*head)
+	if (*head == NULL)
 		return (1);
+
 	/*count how many elements there are*/
-	while (current->next)
+	while (current)
 	{
 		count++;
 		current = current->next;
 	}
 
 	arr = malloc(sizeof(int) * count);
-	if (!arr)
+	if (arr == NULL)
 		return (0);
 
-	for (x = 0; x < count; count++)
+	current = *head;
+
+	for (x = 0; x < count; x++)
 	{
 		arr[x] = current->n;
 		current = current->next;
 	}
 
+	for (x = 0; x < count / 2; x++)
+	{
+		if (arr[x] != arr[count - (x + 1)])
+		{
+			free(arr);
+			return (0);
+		}
+	}
+	free(arr);
+	return (1);
 }
