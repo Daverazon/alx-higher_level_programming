b66126267fbcf17ab3f9079a327999ca8ad321d2
David <kamsidavid2@gmail.com>
2023-09-13 23:32:17 +0000

    refactor code

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    add, div = 0, 0
+    average, add, div = 0, 0, 0
     if my_list:
         mul = [x * y for x, y in my_list]
         for row in my_list:
             div += row[1]
         for m in mul:
             add += m
-    return add / div
+    average = add / div
+    return average
