b2f279b3f5f980f548d3d9875559734d4f9d4001
David <kamsidavid2@gmail.com>
2023-10-06 02:43:31 +0000

    add soln to task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,4 +2,12 @@
 
 class Square:
     """defines a square"""
+    def __init__(self, size):
+    "Initialises a Square object
 
+    Args:
+        size (int): the first parameter
+
+    Returns:
+        nothing
+        self.__size = size
