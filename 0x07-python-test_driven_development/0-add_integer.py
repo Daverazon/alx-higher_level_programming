2c68e67d50a8961ea9162197c84c4c26466c82d1
David <kamsidavid2@gmail.com>
2023-10-10 22:41:51 +0000

    add soln to task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-'''Define an addition function'''
+'''Define an addition function
+
+>>> add_integer(5, 10)
+15
+'''
 
 
 def add_integer(a, b=98):
-    '''Add two numbers
-    
-    Examples:
-        >>> add_integer(5)
-        103
-    Args:
-        a (int): first number
-        b (int): second number 
-    Returns:
-        int: sum of the two numbers
     '''
-    if not(isinstance(a, int) or isinstance(a, float)):
+    Add two numbers
+    '''
+    if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     else:
         a = int(a)
-    if not(isinstance(b, int) or isinstance(b, float)):
+    if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
     else:
         b = int(b)
