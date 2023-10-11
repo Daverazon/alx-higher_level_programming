7c5133b1fa8a3883ce7bf525fb82cc639ee58c08
DavidI <kamsidavid2@gmail.com>
2023-10-11 21:08:16 +0100

    add soln to task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Defines a square printing function
+
+>>> print_square(2)
+##
+##
+'''
+
+
+def print_square(size):
+    '''Prints a square with the character #'''
+
+    if not isinstance(size, int) or isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    else:
+        for row in range(size):
+            for column in range(size):
+                print('#', end='')
+            print('')
