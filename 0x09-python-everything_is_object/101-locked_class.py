7cd6442978fc3c94cbbe24178994683fe05ef01b
David <kamsidavid2@gmail.com>
2023-10-13 08:10:15 +0000

    cleanup code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,10 +4,12 @@
 
 class LockedClass:
     '''Represent a LockedClass
-    
+
     Note:
         user can only dynamically create new instance attribute
         if it is called `first_name`.'''
-    
-    def __init__(self):
-        
+    __slots__ = ("first_name", )
+
+    def __init__(self, first_name=None):
+        '''Initialize a LockedClass'''
+        self.first_name = first_name
