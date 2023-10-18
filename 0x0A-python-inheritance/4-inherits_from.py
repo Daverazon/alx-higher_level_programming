5ec77d471e58a20dfdf511079ba74408d78e0636
DavidI <kamsidavid2@gmail.com>
2023-10-18 21:18:42 +0100

    add soln to task 2 - 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''''''
+
+
+def inherits_from(obj, a_class):
+    '''returns True if the object is an instance
+    of a class that inherited (directly or indirectly) from the specified class
+    otherwise False'''
+    if (isinstance(obj, a_class) and a_class is not type(obj)):
+        return True
+    return False
