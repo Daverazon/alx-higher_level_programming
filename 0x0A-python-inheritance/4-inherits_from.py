2b3a3dbbaeed7f99538c8ab72b7f4a2791954f77
DavidI <kamsidavid2@gmail.com>
2023-10-19 14:50:19 +0100

    add completed tasks

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''''''
+'''Defines an inheritance checker function'''
 
 
 def inherits_from(obj, a_class):
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Defines a BaseGeometry class'''
+
+
+class BaseGeometry:
+    '''Represent a BaseGeometry class'''
+    def area(self):
+        '''Raises an exception'''
+        raise Exception("area() not implemented")
