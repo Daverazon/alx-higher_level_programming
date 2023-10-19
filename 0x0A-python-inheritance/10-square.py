896499d530df1899671fad10a602807fa31c39b4
DavidI <kamsidavid2@gmail.com>
2023-10-19 18:40:14 +0100

    add soln to tasks 10 and 11

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,7 +6,7 @@ class BaseGeometry:
     '''Represent a BaseGeometry class'''
     def area(self):
         '''Raises an exception'''
-        raise Exception("area() is not implemented")
+        raise Exception("area() not implemented")
 
     def integer_validator(self, name, value):
         '''Validates type and value of the argument "value".'''
@@ -20,8 +20,30 @@ class Rectangle(BaseGeometry):
     '''Represent a rectangle class'''
 
     def __init__(self, width, height):
-        '''Initialize a rectangle class'''
+        '''Initialize a BaseGeometry class'''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        '''returns area of a rectangle'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        """prints informal representation of object"""
+        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    '''Represent a square class'''
+
+    def __init__(self, size):
+        '''Initialize a square class'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        '''returns area of a square'''
+        return self.__size ** 2
