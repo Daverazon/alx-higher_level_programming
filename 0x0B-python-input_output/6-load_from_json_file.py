6a6aca5f7cda881f0db12c62293a79a84670494f
DavidI <kamsidavid2@gmail.com>
2023-10-27 20:25:21 +0100

    add soln to task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This script creates an object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """create an object from a JSON file"""
+    with open(filename, encoding='utf-8') as afile:
+        return(json.load(afile))
