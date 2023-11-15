6eda3b91c524ad9ef6e2e867ba35a5666d07ecaf
David <kamsidavid2@gmail.com>
2023-11-15 19:06:20 +0000

    add soln to all mandatory tasks

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use Bob’s id value, only the name field
+USE hbtn_0c_0;
+UPDATE second_table
+SET score = 10 WHERE name = 'Bob';
