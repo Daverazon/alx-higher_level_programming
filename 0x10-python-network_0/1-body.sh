ffc1440e6448f378f880b2cec22117230175229e
DavidI <kamsidavid2@gmail.com>
2024-04-15 00:05:29 +0100

    Create a bash script that  takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
+#!/usr/bin/bash
+# It takes in a URL, sends a GET request to the URL, and displays the body of the response. -L option follows redirects till the final page.
 curl -sL "$1"
