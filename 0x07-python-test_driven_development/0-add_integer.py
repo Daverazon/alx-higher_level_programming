6a572d0585ca773f11fe7323ff7c864d57167b4f
DavidI <kamsidavid2@gmail.com>
2024-06-19 00:02:15 +0100

    create integer addition function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-'''Define an addition function
-
->>> add_integer(5, 10)
-15
-'''
-
+"""Defines an integer addition function"""
 
 def add_integer(a, b=98):
-    '''
-    Add two numbers
-    '''
-    if not (isinstance(a, int) or isinstance(a, float)):
-        raise TypeError("a must be an integer")
-    else:
-        a = int(a)
-    if not (isinstance(b, int) or isinstance(b, float)):
-        raise TypeError("b must be an integer")
-    else:
-        b = int(b)
+    """
+    Returns the integer sum of two values
 
-    return a + b
+    Float arguments are first casted to integers
+    """
+    ab = (['a', a], ['b', b])
+    for x in ab:
+        if not (isinstance(x[1], int) or isinstance(x[1], float)):
+            raise TypeError(f'{x[0]} must be an integer')
+
+        if isinstance(x[1], float):
+            x[1] = int(x[1])
+
+    return ab[0][1] + ab[1][1]
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,18 +1,1 @@
 #!/usr/bin/python3
-'''Text printing function'''
-
-
-def text_indentation(text):
-    '''prints a text with 2 new lines after
-    each of these characters: ., ? and :'''
-
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-    else:
-        for letter in text:
-            print(letter, end='')
-            for sign in ['.', '?', ':']:
-                if letter == sign:
-                    print('')
-                    print('')
-text_indentation('Holberton. School? How are you: John')
