4188fb699a1e2678d94fd1cb85d7ca3ec4c053a4
DavidI <kamsidavid2@gmail.com>
2024-06-21 20:59:50 +0100

    write part of the matrix division function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,4 +16,14 @@ def matrix_divided(matrix, div):
     Returns:
         new matrix with the elements divided and rounded to 2 decimal places
     """
+    matrixError1 = 'matrix must be a matrix (list of lists) of integers/floats'
+    if not isinstance(matrix, list):
+        raise TypeError(matrixError1)
+    if not (isinstance(div, int) and isinstance(div, float)):
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    for rows in matrix:
+        if not isinstance(rows, list) and isinstance(:
+            raise TypeError(matrixError1)
     pass
