a5e2dabf80159814291e789bbf8333287e21e96c
DavidI <kamsidavid2@gmail.com>
2024-06-23 22:53:24 +0100

    add documentation for text indentation function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Defines a text indentation function"""
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and : without leaving any space at the beginning or at the end of each printed line
+
+    Usage:
+        text_indentation(text)
+    Args:
+        text (str)
+    Raises:
+        TypeError: if text is not a string
+    Returns:
+        None
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
