c7e8e871f9a47c819d120157e94ff44f39ae5a2c
DavidI <kamsidavid2@gmail.com>
2024-06-23 22:44:39 +0100

    complete square printing function and update its doctest

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,5 +17,12 @@ def print_square(size):
     Returns:
         None
     """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
-    pass
+    for row in range(size):
+        for column in range(size):
+            print("#", end="")
+        print("")
