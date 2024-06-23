258478005de3a9836fcd20624b336a7794144ac8
David <kamsidavid2@gmail.com>
2024-06-23 20:42:14 +0000

    add documentation for square printing function

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Defines a function that prints a square with the # character"""
+
+
+def print_square(size):
+    """
+    prints a square with the character #
+
+    Usage:
+        print_square(size)
+    Args:
+        size (int): length of the square
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than zero
+        TypeError: if size is a float and is less than zero
+    Returns:
+        None
+    """
+
+    pass
